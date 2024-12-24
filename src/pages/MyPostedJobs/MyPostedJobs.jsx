53cf8c878cf4ee3ba39902fbc7a6e37013604e09
mdashraful24 <mdashrafulislam2882@gmail.com>
2024-12-24 20:48:21 +0600

    added my posted jobs and view applications section

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 const MyPostedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/jobs?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [user.email])
+
     return (
         <div>
-            <h2>My Posted Jobs</h2>
+            <h2 className="text-3xl">My Posted Jobs: {jobs.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Job Title</th>
+                            <th>Company</th>
+                            <th>Deadline</th>
+                            <th>Application Count</th>
+                            <th>Applications</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{job.title}</td>
+                                <td>{job.company}</td>
+                                <td>{job.applicationDeadline}</td>
+                                <td>{job.applicationCount}</td>
+                                <td>
+                                    <Link to={`/viewApplications/${job._id}`}>
+                                        <button className="btn btn-link">
+                                            View Applications
+                                        </button>
+                                    </Link>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -11,6 +11,7 @@ import JobApply from "../pages/JobApply/JobApply";
 import MyApplications from "../pages/MyApplications/MyApplications";
 import AddJob from "../pages/AddJob/AddJob";
 import MyPostedJobs from "../pages/MyPostedJobs/MyPostedJobs";
+import ViewApplications from "../pages/ViewApplications/ViewApplications";
 
 const router = createBrowserRouter([
     {
@@ -52,6 +53,13 @@ const router = createBrowserRouter([
                 element: <PrivateRoute>
                     <MyPostedJobs></MyPostedJobs>
                 </PrivateRoute>
+            },
+            {
+                path: "viewApplications/:job_id",
+                element: <PrivateRoute>
+                    <ViewApplications></ViewApplications>
+                </PrivateRoute>,
+                loader: ({ params }) => fetch(`http://localhost:5000/job-applications/jobs/${params.job_id}`)
             },
             {
                 path: "register",
